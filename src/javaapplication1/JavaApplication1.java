c9cb914e83c91857b650c90f09cf39c06f617f20
Pablo Benavides <Pablo Benavides@DESKTOP-JTL8OO3>
2023-06-07 11:22:26 -0600

    Ejercicio 1

---
+++ src/javaapplication1/JavaApplication1.java
@@ -0,0 +1,28 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package javaapplication1;
+
+/**
+ *
+ * @author LABORATORIO 04
+ */
+public class JavaApplication1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+int ancho=10;
+int alto=5;
+
+for (int i=0; i<alto; i++) {
+   for (int j=0; j<ancho; j++) {
+      System.out.print("@");
+     }
+     System.out.println(); 
+    }
+  }   
+}
